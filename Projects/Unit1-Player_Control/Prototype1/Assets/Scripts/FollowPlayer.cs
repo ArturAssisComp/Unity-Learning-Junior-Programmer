366d498a89bafbacdf2399e70aba9ddd27991074
ArturAssisComp <arturassiscomp@gmail.com>
2021-04-26 16:00:22 -0300

    Implement the Prototype1

---
+++ Projects/Unit1-Player_Control/Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 OFF_SET_VECTOR = new Vector3(0f, 6.41f, -7.08f); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.transform.position + OFF_SET_VECTOR;
+    }
+}
