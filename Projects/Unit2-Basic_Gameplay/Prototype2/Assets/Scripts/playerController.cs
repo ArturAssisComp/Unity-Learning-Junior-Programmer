81e481e3efe668f3d3b0ef757bcaca36de8c7242
ArturAssisComp <arturassiscomp@gmail.com>
2021-05-05 21:15:00 -0300

    Add elements in the game hierarchy and limit the movement of the player.

---
+++ Projects/Unit2-Basic_Gameplay/Prototype2/Assets/Scripts/playerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //Private attributes:
+    private float speed = 35f;
+    private float horizontalInput;
+    private float leftBound = -20f, rightBound = 20f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Get the horizontal input [-1, +1]:
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //Move the player within the lateral bounds:
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        //Make the player stay inbound:
+        if (transform.position.x < leftBound)
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+        else if (transform.position.x > rightBound)
+            transform.position = new Vector3(rightBound, transform.position.y, transform.position.z);
+    }
+}
