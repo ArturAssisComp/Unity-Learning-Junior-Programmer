e03c67d98a4f15d7e6be9a4da0c0bb9a616bcb69
ArturAssisComp <arturassiscomp@gmail.com>
2021-05-10 17:11:41 -0300

    Finish challenge 2

---
+++ Projects/Unit2-Basic_Gameplay/Challenge2-Play_fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float minDelay = 2f, timeElapsed = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update timeElapsed:
+        timeElapsed += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeElapsed >= minDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            //Reset timeElapsed:
+            timeElapsed = 0;
+        }
+    }
+}
