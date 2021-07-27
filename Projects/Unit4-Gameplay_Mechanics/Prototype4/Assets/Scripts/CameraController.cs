99022414bef770cf8ab64c2d138e1c84aea6570f
ArturAssisComp <arturassiscomp@gmail.com>
2021-07-27 16:01:51 -0300

    Implement the basics of player, camera, and enemy

---
+++ Projects/Unit4-Gameplay_Mechanics/Prototype4/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Attributes:
+    public float rotationSpeed = 50f;
+    private float input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the user's input:
+        this.input = Input.GetAxis("Horizontal");
+
+        //Move the camera:
+        this.transform.Rotate(Vector3.up * this.rotationSpeed * Time.deltaTime * this.input);
+    }
+}
---
+++ Projects/Unit4-Gameplay_Mechanics/Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Atributes:
+    public float force = 5;
+    private float input;
+    private Rigidbody rigidBody;
+    private GameObject CameraCenterPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the player's components:
+        this.rigidBody = this.gameObject.GetComponent<Rigidbody>();
+
+        //Get the references to GameObject:
+        this.CameraCenterPoint = GameObject.Find("Camera Center Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get inputs:
+        this.input = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if(this.input != 0)
+        {
+            //Move the player:
+            this.rigidBody.AddForce(this.CameraCenterPoint.transform.forward * this.input * this.force, ForceMode.Force);
+        }
+    }
+
+
+
+}
