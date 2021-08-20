af07c87eb96add317960ccddba54dfd6b0643f56
ArturAssisComp <arturassiscomp@gmail.com>
2021-08-20 16:48:54 -0300

    Start prototype 5. Good and bad objects were implemented

---
+++ Projects/Unit5-User_Interface/Prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    //Attributes:
+    private float minForce = 8f,  minTorque = -10f, minHorizontalPosition = -4f;
+    private float maxForce = 15f, maxTorque = 10f,  maxHorizontalPosition =  4f;
+    private float initialVerticalPosition = -1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the rigid body:
+        Rigidbody rigidbody = this.gameObject.GetComponent<Rigidbody>();
+
+        //Apply the initial conditions:
+        this.transform.position = new Vector3(this.GetRandomHorizontalPosition(), this.initialVerticalPosition);
+        rigidbody.AddForce(Vector3.up * this.GetRandomForce(), ForceMode.Impulse);
+        rigidbody.AddTorque(this.GetRandomTorque(), this.GetRandomTorque(), this.GetRandomTorque(), ForceMode.Impulse);
+        
+        
+    }
+
+
+    private float GetRandomHorizontalPosition()
+    {
+        return Random.Range(this.minHorizontalPosition, this.maxHorizontalPosition);
+    }
+
+    private float GetRandomForce()
+    {
+        return Random.Range(this.minForce, this.maxForce);
+    }
+
+    private float GetRandomTorque()
+    {
+        return Random.Range(this.minTorque, this.maxTorque);
+    }
+
+
+}
